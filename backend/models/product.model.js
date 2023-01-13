d30d7925904f31b8d882ecd4b32a16adbae6ba46
dhruvilxcode <dhruvilxcode@gmail.com>
2023-01-13 12:15:45 +0530

    Product model added.

---
+++ backend/models/product.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide product name'],
+        trim: true,
+        maxLength: [120, 'Product name should be less than 120 character'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please provide price'],
+        maxLength: 10,
+    },
+    description: {
+        type: String // use editorjs or similar
+    },
+    photos: [
+        {
+            secure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    collectionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Collection"
+    },
+}, {timestamps: true});
+
+export default mongoose.model("Product", productSchema);
