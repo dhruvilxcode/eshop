4790a9043a72cf8e92ab1b617390cf4f55ef2477
dhruvilxcode <dhruvilxcode@gmail.com>
2023-01-17 13:05:20 +0530

    collection controller added.

--- backend/controllers/collection.controller.js
+++ backend/controllers/collection.controller.js
@@ -27,3 +27,47 @@ export const createCollection = async (req, res) => {
         collection
     });
 }
+
+export const updateCollection = async (req, res) => {
+    const {id: collectionId} = req.params;
+
+    const {name} = req.body;
+
+    if(!name) {
+        return res.status(400).json({
+            message: "Collection name is required"
+        });
+    }
+
+    const modifiedCollection = await Collection.findByIdAndUpdate(collectionId, {
+        name
+    }, {new: true, runValidators: true});
+
+    return res.status(200).json({
+        success: true,
+        message: "Collection updated.",
+        collection: modifiedCollection
+    });
+};
+
+export const deleteCollection = async (req, res) => {
+    const {id: collectionId} = req.params;
+
+    const collection = await Collection.findByIdAndDelete(collectionId);
+    collection.remove();
+
+    return res.status(200).json({
+        success: true,
+        message: "Collection removed successfully."
+    });
+}
+
+export const getAllCollection = async (req, res) => {
+    const collections = await Collection.find();
+
+    if(!collections) {
+        return res.status(200).json([]);
+    }
+
+    return res.status(200).json(collections);
+}
