a81fc18d1c373e0abaf0a73f156241bbaccdb715
dhruvilxcode <dhruvilxcode@gmail.com>
2023-01-19 10:43:05 +0530

    deactivate coupon controller added.

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -18,6 +18,17 @@ export const createCoupon = async (req, res) => {
     }
 
     try {
+        // check if coupon already exist
+        const couponExists = await Coupon.findOne({
+            code
+        });
+
+        if(couponExists) {
+            return res.status(400).json({
+                message: "oops! Coupon code already exists!"
+            });
+        }
+
         const coupon = await Coupon.create({
             code,
             discount: parseFloat(discount), 
@@ -45,7 +56,40 @@ export const createCoupon = async (req, res) => {
  * @description Only admin and Moderator can update the coupon
  * @returns Coupon Object with success message "Coupon Deactivated SuccessFully"
  *********************************************************/
+export const deactivateCoupon = async (req, res) => {
+    const { code } = req.params;
 
+    try {
+        // check if coupon don't exist
+        const couponExists = await Coupon.findOne({
+            code
+        });
+
+        if(!couponExists) {
+            return res.status(404).json({
+                message: "oops! provided coupon is no longer available!"
+            });
+        }
+
+        const coupon = await Coupon.findOneAndUpdate({
+            code, 
+        }, {
+            active: false,
+        }, {new: true});
+
+        return res.status(201).json({
+            success: true,
+            message: "Coupon deactivated.",
+            coupon
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while saving details."
+        });
+    }
+}
 
 /**********************************************************
  * @DELETE_COUPON
